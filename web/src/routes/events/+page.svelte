40f61e4d71a53d7b8d7a76f06b55d82afaec9f28
d34dscene <d34dscene@pm.me>
2025-11-20 01:37:25 +0100

    add events

---
+++ web/src/routes/events/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import * as Empty from '$lib/components/ui/empty/index.js';
+	import { CheckIcon } from '@lucide/svelte';
+
+	let incidents = [];
+</script>
+
+<div class="mx-auto w-full space-y-6 p-6 sm:max-w-3xl">
+	{#if incidents.length === 0}
+		<Empty.Root class="border border-dashed">
+			<Empty.Header>
+				<Empty.Media variant="icon">
+					<CheckIcon class="text-primary" />
+				</Empty.Media>
+				<Empty.Title>No incidents found</Empty.Title>
+				<Empty.Description>Everything is working as expected</Empty.Description>
+			</Empty.Header>
+		</Empty.Root>
+	{:else}
+		<div class="flex flex-col gap-4">
+			<!-- {#each incidents as incident (incident.id)} -->
+			<!-- 	<StatusCard {incident} /> -->
+			<!-- {/each} -->
+		</div>
+	{/if}
+</div>
