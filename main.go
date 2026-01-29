9261b52c8b5fb1e85b5cec4946361b91604fb273
d34dscene <d34dscene@pm.me>
2026-01-29 18:53:28 +0100

    feat(main): move main files

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/mizuchilabs/beacon/internal/api"
+	"github.com/mizuchilabs/beacon/internal/config"
+	"github.com/urfave/cli/v3"
+)
+
+var (
+	Version = "unknown"
+	Commit  string
+	Date    string
+	Dirty   string
+)
+
+func main() {
+	cmd := &cli.Command{
+		EnableShellCompletion: true,
+		Suggest:               true,
+		Name:                  "beacon",
+		Version:               Version,
+		Usage:                 "beacon [command]",
+		Description:           `Beacon is a simple uptime monitoring tool for websites.`,
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			cfg := config.New(ctx, cmd)
+			return api.NewServer(cfg).Start(ctx)
+		},
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:    "version",
+				Aliases: []string{"v"},
+				Usage:   "Print version information",
+			},
+			&cli.BoolFlag{
+				Name:    "debug",
+				Aliases: []string{"d"},
+				Usage:   "Enable debug logging",
+				Sources: cli.EnvVars("BEACON_DEBUG"),
+			},
+			&cli.StringFlag{
+				Name:    "port",
+				Aliases: []string{"p"},
+				Usage:   "Server port",
+				Value:   "3000",
+				Sources: cli.EnvVars("BEACON_PORT"),
+			},
+			&cli.StringFlag{
+				Name:    "config",
+				Aliases: []string{"c"},
+				Usage:   "Path to monitors config file",
+				Value:   "config.yaml",
+				Sources: cli.EnvVars("BEACON_CONFIG"),
+			},
+			&cli.StringFlag{
+				Name:    "chart-type",
+				Aliases: []string{"t"},
+				Usage:   "Chart type (bars or area)",
+				Value:   "area",
+				Sources: cli.EnvVars("BEACON_CHART_TYPE"),
+			},
+		},
+	}
+
+	// Graceful shutdown
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	if err := cmd.Run(ctx, os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
